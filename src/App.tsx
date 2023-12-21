70e62055c9a7bf08b5e1fece2bd61c4e5002d9d9
vel-3633f <vel3633fu@gmail.com>
2023-12-21 12:04:50 +0900

    検索機能実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState<User[]>([]);
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const handleChange = () => {
+    console.log(users, searchQuery, ref.current.value);
+
+    if (!ref.current) {
+      setSearchQuery(users);
+      return; // ref.current が null の場合、何もせずに終了
+    }
+    //フィルタリング機能
+    setSearchQuery(
+      users.filter((user) => {
+        return user.name.toLowerCase().includes(ref.current.value);
+      })
+    );
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((json) => setUsers(json));
+      .then((json) => {
+        setUsers(json);
+        setSearchQuery(json);
+      });
   }, []);
 
-  console.log(users);
   return (
     <div className="App">
       <div className="main">
         <h2>検索アプリ</h2>
-        <input type="text" />
+        <input type="text" ref={ref} onChange={() => handleChange()} />
         <div className="content">
-          {users.map((user) => (
-            <div className="box">
-              <h3>ユーザー名</h3>
+          {searchQuery.map((user) => (
+            <div className="box" key={user.id}>
+              <h3>{user.name}</h3>
               <hr />
-              <p>メールアドレス</p>
+              <p>{user.email}</p>
             </div>
           ))}
         </div>
